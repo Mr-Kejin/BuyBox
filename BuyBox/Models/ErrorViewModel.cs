9a8a02a1d8f5b065a1cdb5046c8988ba754895b6
Kejin_Anit <kejin.ajit@micrologic.net>
2023-12-31 21:32:38 +0530

    Add project files.

---
+++ BuyBox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BuyBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
