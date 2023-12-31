eb6a514bb57313b0e0315b6702c96742274ffff7
Kejin_Anit <kejin.ajit@micrologic.net>
2023-12-31 22:40:18 +0530

    setting up with the environment

---
+++ BuyBox/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BuyBox.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuyBox.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
