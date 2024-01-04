2622e8041165473d178a16edef9443f4162fba24
Kejin_Anit <kejin.ajit@micrologic.net>
2024-01-04 23:04:20 +0530

    Day4: made some ui and valiation changess

--- BuyBox/Areas/Admin/Controllers/ProductController.cs
+++ BuyBox/Areas/Admin/Controllers/ProductController.cs
@@ -35,16 +35,25 @@ namespace BuyBox.Areas.Admin.Controllers
             return View(productVM);
         }
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(ProductVM productVM)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["Success"] = "product created successfully";
                 return RedirectToAction("Index");
             }
-            return View();
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });                   
+                return View(productVM);
+
+            }
         }
         public IActionResult Edit(int? id)
         {
