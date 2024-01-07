49328b1862d4024794b49bf93fcc8456b9a28381
Kejin_Anit <kejin.ajit@micrologic.net>
2024-01-07 13:21:35 +0530

    Day 5: fixed the issues in the product  when an image is added its not deleting the existing image and uploaded images for all the products, and worked on the upsert function and initializesd data table for the products menu and verified the functiinalaity

--- BuyBox/Areas/Admin/Controllers/ProductController.cs
+++ BuyBox/Areas/Admin/Controllers/ProductController.cs
@@ -11,14 +11,16 @@ namespace BuyBox.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return View(objProductList);
         }
         public IActionResult Upsert(int? id)
@@ -49,7 +51,36 @@ namespace BuyBox.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(productVM.Product);
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file!= null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\products");
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        //Delete the old image and upload the new image 
+                        var oldImagePath = 
+                            Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                           System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+                    using (var fileStream =new FileStream(Path.Combine(productPath, fileName),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productVM.Product.ImageUrl = @"\images\products\" + fileName;
+                }
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
                 _unitOfWork.Save();
                 TempData["Success"] = "product created successfully";
                 return RedirectToAction("Index");
@@ -94,5 +125,13 @@ namespace BuyBox.Areas.Admin.Controllers
 
 
         }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new {data = objProductList});
+        }
+        #endregion
     }
 }
---
+++ BuyBox/wwwroot/js/product.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/Admin/Product/getall' },
+        "columns": [
+            { data: 'title',"width": "15%" },
+            { data: 'listPrice', "width": "15%" },
+            { data: 'category.name', "width": "15%" },
+        ]
+    });
+}
+
