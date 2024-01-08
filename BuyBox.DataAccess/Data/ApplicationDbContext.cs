b70a3095263214b8218425bcf64712163e63a367
Kejin_Anit <kejin.ajit@micrologic.net>
2024-01-08 19:22:55 +0530

    Day6: Added new scuffoling for validation ogin page and registeration pages, creaed UI for login and register page, created required tables for  login and regster page, added 2step authentication for login, register and logout. created table for identity, user roles and login and log out

--- BuyBox.DataAccess/Data/ApplicationDbContext.cs
+++ BuyBox.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using BuyBox.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BuyBox.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext <IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -14,6 +16,9 @@ namespace BuyBox.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+
             modelBuilder.Entity<Category>().HasData(
                new Category { Id = 1, Name = "Sand", DisplayOrder = 1 },
                new Category { Id = 2, Name = "Brick", DisplayOrder = 2 },
